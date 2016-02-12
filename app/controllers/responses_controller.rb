499e5383bdbd26bfa3839a0e479c75c7ffd879ab
Kristie Chow <email": null>
2016-02-12 16:14:17 -0500

    Add response partials

---
+++ app/controllers/responses_controller.rb
@@ -0,0 +1,44 @@
+class ResponsesController < ApplicationController
+
+  def create
+    @question = Question.find_by(id: params[:question_id])
+    # @answer = Answer.find_by(id: params[:answer_id])
+
+    new_response = Response.new(response_params)
+
+    if new_response.save
+      flash.notice = "Response saved!"
+      redirect_to questions_path(@question)
+    else
+      flash.alert = "Response not saved, sorry!"
+      redirect_to questions_path(@question)
+    end
+  end
+
+  # def edit
+  #   @response = response.find_by(id: params[:id])
+  # end
+
+  # def update
+  #   @response = response.find_by(id: params[:id])
+  #   if @response.update(response_params)
+
+  #   else
+
+  #   end
+  # end
+
+  def destroy
+    @response = response.find_by(id: params[:id])
+    @response.destroy
+    flash.notice = "Response Deleted"
+  end
+
+  private
+
+  def response_params
+    params.require(:response).permit(:content, :user_id, :respondable_type, :respondable_id)
+  end
+
+end
+
--- app/models/response.rb
+++ app/models/response.rb
@@ -1,3 +1,4 @@
 class Response < ActiveRecord::Base
   belongs_to :respondable, polymorphic: true
+
 end

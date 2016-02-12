ab97f86bd89d95d49f9a8ef9da876db2fd1b5101
Kristie Chow <email": null>
2016-02-12 10:14:48 -0500

    Update routes

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,19 +1,11 @@
 class AnswersController < ApplicationController
 
-  def index
-    @answers = Answer.where(question_id: params[:question_id])
-  end
-
   def new
     @answer = Answer.new
   end
 
   def create
     @answer = Answer.create(answer_params)
-  end
-
-  def show
-    @answer = Answer.find_by(id: params[:id])
   end
 
   private

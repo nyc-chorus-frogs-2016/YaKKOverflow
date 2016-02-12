6e5dd9fb60056f236c4031dae9f9f5e7a6fcacbb
Yi Lu <email": null>
2016-02-12 14:15:57 -0500

    Finish testmvp without seeds

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,25 +1,44 @@
 class AnswersController < ApplicationController
 
   def create
-    @answer = Answer.create(answer_params)
+    @question = Answer.find_by(id: params[:question_id])
+    @answer = Answer.new(answer_params)
+    if @answer.save
+      flash.notice = "Answer Created"
+      redirect_to question_path(@question)
+    else
+      redirect_to question_path(@question)
+    end
   end
 
   def edit
-  end
-
-  def show
+    @question = @question = Answer.find_by(id: params[:question_id])
+    @answer = Answer.find_by(id: params[:id])
   end
 
   def update
+    @question = Answer.find_by(id: params[:question_id])
+    @answer = Answer.find_by(id: params[:id])
+    if @answer.update(answer_params)
+      flash.notice = "Answer Updated"
+      redirect_to question_path(@question)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @question = Answer.find_by(id: params[:question_id])
+    @answer = answer.find_by(id: params[:id])
+    @answer.destroy
+    flash.notice = "Answer Deleted"
+    redirect_to  question_path(@question)
   end
 
   private
 
   def answer_params
-    params.require(:answer).permit(:content)
+    params.require(:answer).permit(:content, :question_id).merge(user: current_user)
   end
 
 end
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,9 @@
 class Answer < ActiveRecord::Base
   belongs_to :question
   belongs_to :user
+
+  def editable_by? user
+    creator == user
+  end
+
 end

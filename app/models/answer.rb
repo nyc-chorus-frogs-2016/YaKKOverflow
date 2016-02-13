96d79d818f653c53840a0597a8c651eb9a5196bf
Yi Lu <email": null>
2016-02-13 11:05:24 -0500

    Finish Votes for Answers

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -8,4 +8,20 @@ class Answer < ActiveRecord::Base
     self.user == user
   end
 
+  def vote_sum
+    Vote.where("votable_type = ? AND votable_id =?", "Answer", self.id).pluck("value").inject(0){|sum,x| sum + x }
+  end
+
+  def already_voted?(user_id)
+    !Vote.where("votable_type = ? AND user_id = ? AND votable_id = ?", "Answer", user_id, self.id).empty?
+  end
+
+  def answer_up_or_down_vote(user_id)
+    if Vote.where("votable_type = ? AND user_id = ? AND votable_id = ?", "Answer", user_id, self.id)[0].value == 1
+      return "up"
+    else
+      return "down"
+    end
+  end
+
 end
